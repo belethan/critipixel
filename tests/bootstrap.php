e7de9e6c68dbe60bf18b86a881ea33fc54ae3227
ludohome <ludovic@coquemert.fr>
2025-12-03 18:24:20 +0100

    Double vérification des fichiers car beaucoup de tests en echecs.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,18 +4,18 @@ declare(strict_types=1);
 
 use Symfony\Component\Dotenv\Dotenv;
 
-require dirname(__DIR__).'/vendor/autoload.php';
+require dirname(__DIR__) . '/vendor/autoload.php';
 
 $dotenv = new Dotenv();
 
 /*
  * CI GitHub Actions → SQLite → .env.test
- * Local → .env (MySQL)
+ * Local → MySQL → .env
  */
 if ('true' === getenv('GITHUB_ACTIONS')) {
-    $dotenv->bootEnv(dirname(__DIR__).'/.env.test');
+    $dotenv->bootEnv(dirname(__DIR__) . '/.env.test');
 } else {
-    $dotenv->bootEnv(dirname(__DIR__).'/.env');
+    $dotenv->bootEnv(dirname(__DIR__) . '/.env');
 }
 
 if ($_SERVER['APP_DEBUG'] ?? false) {
