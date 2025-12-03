c7c10d17fcee73752422e2024061215f9713fd4e
ludohome <ludovic@coquemert.fr>
2025-12-03 16:58:45 +0100

    Changement N fois du fichier config pour la gestion des tests database

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,24 +4,20 @@ declare(strict_types=1);
 
 use Symfony\Component\Dotenv\Dotenv;
 
-require dirname(__DIR__) . '/vendor/autoload.php';
+require dirname(__DIR__).'/vendor/autoload.php';
 
 $dotenv = new Dotenv();
 
-/**
+/*
  * CI GitHub Actions → SQLite → .env.test
  * Local (pas GitHub) → MySQL → .env
  */
-if (getenv('GITHUB_ACTIONS') === 'true') {
-    // CI: on charge .env.test (SQLite)
-    $dotenv->bootEnv(dirname(__DIR__) . '/.env.test');
+if ('true' === getenv('GITHUB_ACTIONS')) {
+    $dotenv->bootEnv(dirname(__DIR__).'/.env.test');
 } else {
-    // Local: on charge .env normalement (MySQL)
-    $dotenv->bootEnv(dirname(__DIR__) . '/.env');
+    $dotenv->bootEnv(dirname(__DIR__).'/.env');
 }
 
-// Reproduit la config Symfony pour APP_DEBUG
 if ($_SERVER['APP_DEBUG'] ?? false) {
     umask(0000);
 }
-
